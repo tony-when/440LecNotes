033ba34e755601a624abc9abb65191ef2a608fab
Michael Lee <lee@soi2.org>
2024-04-27 00:35:54 -0500

    Updating call/cc lecture

--- 11-callcc.rkt
+++ 11-callcc.rkt
@@ -24,20 +24,28 @@ the result of `proc` is the result of `call/cc`.
 
 ;; evaluate & explain:
 
-; (* 10 (call/cc (lambda (_) 20)))
+#; (* 10 (call/cc (lambda (_) 20)))
 
-; (* 10 (call/cc (lambda (k) (k 20))))
+#; (* 10 (call/cc (lambda (k) (k 20))))
 
-; (* 10 (call/cc (lambda (k) (k (* 2 10)))))
+#; (* 10 (call/cc (lambda (k) (k (* 2 10)))))
 
-; (* 10 (call/cc (lambda (k) (* 2 (k 10)))))
+#; (* 10 (call/cc (lambda (k) (* 2 (k 10)))))
 
 
 ;; we can save a continuation!
 (define *k* void)
 
-(define (arith x)
-  (* x 10))
+(define (arith x y z)
+  (+ x (* y (/ z 10))))
+
+
+(define (sum-to n)
+  (if (= n 0)
+      0
+      (+ n (sum-to (sub1 n)))))
+
+(trace sum-to)
 
 
 #|-----------------------------------------------------------------------------
@@ -54,33 +62,10 @@ the result of `proc` is the result of `call/cc`.
 ;; ... as an escape hatch
 -----------------------------------------------------------------------------|#
 
-;; short-circuit break/return
-(define (find pred lst)
-  (cond [(empty? lst) (void)]
-        [(pred (first lst)) (first lst)]
-        [else (find pred (rest lst))]))
-  
-
-#|-----------------------------------------------------------------------------
-;; ... for implementing co-routines
------------------------------------------------------------------------------|#
-
-(define (ping k n)
-  (sleep 1)
-  (let loop ([i 1])
-    (println (format "ping ~a" i))
-    (k ping)
-    (when (< i n)
-      (loop (add1 i)))))
-
-(define (pong k)
-  (sleep 1)
-  (let loop ()
-    (println "pong")
-    (k pong 10)
-    (loop)))
-
-(trace ping pong)
+(define (prod lst)
+  (trace-let p ([lst lst])
+    (cond [(empty? lst) 1]
+          [else (* (first lst) (p (rest lst)))])))
 
 
 #|-----------------------------------------------------------------------------
@@ -92,4 +77,45 @@ the result of `proc` is the result of `call/cc`.
 (define-syntax (try stx)
   (syntax-case stx (catch)
     [(_ body ... catch id handler)
-     #'()]))
+     #'(let ([exception (current-continuation)])
+         (if (continuation? exception)
+             (begin
+               (set! *estack* (cons exception *estack*))
+               body ... 
+               (set! *estack* (cdr *estack*))) ; may not get here
+             ((lambda (id) handler) exception)))]))
+
+(define (throw e)
+  (let ([k (car *estack*)])
+    (set! *estack* (cdr *estack*)) ; not very elegant!
+    (k e)))
+
+#;
+(try (println "L1")
+     (println "L2")
+     (println "L3")
+ catch e
+     (println (format "Exception: ~a" e)))
+
+#; 
+(try (println "L1")
+     (println "L2")
+     (throw 'BANG)
+     (println "L3")
+ catch e
+     (println (format "Exception: ~a" e)))
+
+#;
+(try
+     (try (println "L1")
+          (println "L2")
+          (throw 'BANG)
+          (println "L3")      
+      catch e
+          (begin
+            (println (format "Inner: ~a" e))
+            (throw 'BOOM)))
+ catch e
+     (println (format "Outer: ~a" e)))
+
+;; NB: for real implementations, better to use dynamic-wind or dynamic binding
